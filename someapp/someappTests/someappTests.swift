053e9eacafd9f229e80487480f4b1ec59e2ca09c
Olga Emelianova <volha.emelianova@gmail.com>
2019-06-07 01:21:36 +0300

    Add unit test example

--- someapp/someappTests/someappTests.swift
+++ someapp/someappTests/someappTests.swift
@@ -10,18 +10,28 @@ import XCTest
 @testable import someapp
 
 class someappTests: XCTestCase {
+    var sut : ViewControllerBMI!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = ViewControllerBMI()
+        
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let height = 180.0
+        let weight = 75.0
+        XCTAssertEqual(75.0 / (1.8 * 1.8), sut.bmi(heightF: height, weightF: weight))
     }
 
     func testPerformanceExample() {
